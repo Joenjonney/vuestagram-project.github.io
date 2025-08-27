d7f9e3ca1df0573f08adb63c8e068b2f9f47cd75
mac <mac@macui-MacBookAir.local>
2025-08-27 22:35:05 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vuestagram-project.github.io/sw.js', { scope: '/vuestagram-project.github.io/' })})}
